b03f0ce151e3471ea59feb2f5b9081d1262f4c02
Ali Sina <ali.sina1447@gmail.com>
2024-07-16 02:20:04 -0400

    header added

---
+++ pages/components/App.js
@@ -0,0 +1,9 @@
+import Header from "./Header.js"
+
+const App = () => {
+    return (
+        < Header />
+    )
+}
+
+export default App;
---
+++ pages/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="./next.svg"/>
+            </div>
+            <div>
+                company name
+            </div>
+        </header>
+    )
+}
+
+export default Header;
